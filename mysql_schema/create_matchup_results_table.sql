2c1720f63dcdf2e3830c25dc2ea040d8257c5685
TKarim813 <taha_karim57@hotmail.co.uk>
2020-09-08 14:20:56 +0100

    added datetime to matchup results table

--- mysql_schema/create_matchup_results_table.sql
+++ mysql_schema/create_matchup_results_table.sql
@@ -2,8 +2,11 @@ use moviematchupdb;
 
 create table matchup_results (
 id int auto_increment primary key,
+user_id int,
 winner int,
 loser int,
+date_time datetime default now(),
 foreign key(winner) references movies(id),
-foreign key(loser) references movies(id)
+foreign key(loser) references movies(id),
+foreign key(user_id) references users(id)
 );
--- mysql_schema/show_matchup_results.sql
+++ mysql_schema/show_matchup_results.sql
@@ -1,15 +1,18 @@
 use moviematchupdb;
 
 select 
-	user,
+	matchup_results.id,
+    user,
     movies_w.movie as winner,
-    movies_l.movie as loser
+    movies_l.movie as loser,
+    date_time
 from matchup_results
 join users
 	on users.id = matchup_results.user_id
 join movies as movies_w
 	on movies_w.id = matchup_results.winner
 join movies as movies_l
-	on movies_l.id = matchup_results.loser;
+	on movies_l.id = matchup_results.loser
+where user = "kaboom";
     
     
