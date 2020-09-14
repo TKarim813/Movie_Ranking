2a761f3003cae73d63957b1d9ce24da536a75ce2
TKarim813 <taha_karim57@hotmail.co.uk>
2020-09-14 16:13:39 +0100

    added a way to setup database in beginning

---
+++ mysql_schema/setup.sql
@@ -0,0 +1,5 @@
+source C:\Users\Thaha UNI\Documents\Python Projects\Movie Ranking\mysql_schema\create_movie_table.sql
+source C:\Users\Thaha UNI\Documents\Python Projects\Movie Ranking\mysql_schema\create_users_table.sql
+source C:\Users\Thaha UNI\Documents\Python Projects\Movie Ranking\mysql_schema\create_user_rankings_table.sql
+source C:\Users\Thaha UNI\Documents\Python Projects\Movie Ranking\mysql_schema\create_matchup_results_table.sql
+
